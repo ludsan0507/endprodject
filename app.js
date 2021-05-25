c764f3dad8851d6a1397091201e93345c03ce819
ludsan0507 <ludsan0507@gmail.com>
2021-05-25 13:56:07 +0200

    05-25
    13:55

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const port = 3000
 
 const nameList= ["", ""]
@@ -10,16 +11,19 @@ const store = require('./mongodbTest')
 const clientDir = __dirname + "\\client\\"
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(clientDir))
 
-app.get('/style.css', (req, res) => {
-    res.sendFile(clientDir + 'index')
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
 app.post('/', (req,res) => {
 
-    let person = personmodul.createPerson(req.body.name, req.body.email, req.body.age)
-    person.store();
+    let person = personmodul.createPerson(req.body.name, req.body.email, req.body.password)
+    store.storeElement(person);
     res.redirect('/')
 })
 
--- personmodule.js
+++ personmodule.js
@@ -4,17 +4,17 @@ const { stringify } = require('querystring');
 const personSchema = new mongoose.Schema({
     name: String,
     email: String,
-   
+   password: String
 });
   
 const Person = mongoose.model('Person', personSchema);
 
 
-exports.createPerson = (name, email, age) => {
+exports.createPerson = (name, email, password) => {
     var person = new Person({
         name: name, 
         email: email, 
-        
+        password: password,
     })
 
     return person
